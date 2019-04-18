1d52ceb7d937c649e95b69c90cc2df6ead8ff8cc
Fletcher Nichol <fnichol@nichol.ca>
2019-04-17 22:10:28 -0600

    Convert project to use iocage-plugin-cli.
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

--- post_install.sh
+++ post_install.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
 set -eu
 
+plugin services set sshd nginx gitea
+
 gitea_work_dir=/var/db/gitea
-/usr/local/bin/pluginset gitea_work_dir "$gitea_work_dir"
+plugin config set gitea_work_dir "$gitea_work_dir"
 
 gitea_user=git
-/usr/local/bin/pluginset gitea_user "$gitea_user"
+plugin config set gitea_user "$gitea_user"
 
 gitea_user_home="$(getent passwd "$gitea_user" | cut -d : -f 6)"
-/usr/local/bin/pluginset gitea_user_home "$gitea_user_home"
+plugin config set gitea_user_home "$gitea_user_home"
 
 gitea_internal_token="$(gitea generate secret INTERNAL_TOKEN)"
-/usr/local/bin/pluginset gitea_internal_token "$gitea_internal_token"
+plugin config set gitea_internal_token "$gitea_internal_token"
 
 gitea_secret_key="$(gitea generate secret SECRET_KEY)"
-/usr/local/bin/pluginset gitea_secret_key "$gitea_secret_key"
+plugin config set gitea_secret_key "$gitea_secret_key"
 
 gitea_lfs_jwt_secret="$(gitea generate secret LFS_JWT_SECRET)"
-/usr/local/bin/pluginset gitea_lfs_jwt_secret "$gitea_lfs_jwt_secret"
+plugin config set gitea_lfs_jwt_secret "$gitea_lfs_jwt_secret"
 
-/usr/local/bin/pluginset gitea_app_name 'Gitea: Git with a cup of tea'
-/usr/local/bin/pluginset gitea_domain localhost
-/usr/local/bin/pluginset gitea_http_addr 127.0.0.1
-/usr/local/bin/pluginset gitea_protocol http
+plugin config set gitea_app_name 'Gitea: Git with a cup of tea'
+plugin config set gitea_domain localhost
+plugin config set gitea_http_addr 127.0.0.1
+plugin config set gitea_nginx_mode http
 
-mkdir -p "$gitea_work_dir/custom/conf" "$gitea_work_dir/data" "$gitea_work_dir/log"
+mkdir -p "$gitea_work_dir/custom/conf" "$gitea_work_dir/data" \
+  "$gitea_work_dir/log"
 chown -R "$gitea_user" "$gitea_work_dir"
 chmod -R 0750 "$gitea_work_dir"
 echo "Created initial state and data directories under $gitea_work_dir"
@@ -46,9 +49,9 @@ i
 start_precmd="\${name}_prestart"
 
 gitea_prestart() {
-	/usr/local/bin/renderconfig \\
+	/usr/local/bin/plugin template render \\
 		/usr/local/share/gitea/conf/app.ini.in \\
-		$gitea_work_dir/custom/conf/app.ini
+		$gitea_work_dir/custom/conf/app.ini || return 1
 	chmod 0640 $gitea_work_dir/custom/conf/app.ini
 }
 .
@@ -57,22 +60,25 @@ EOEX
   echo "Modified gitea rc.d script with a start_precmd"
 fi
 
-if ! grep -q 'renderconfig' /usr/local/etc/rc.d/nginx >/dev/null; then
+if ! grep -q 'template render' /usr/local/etc/rc.d/nginx >/dev/null; then
   ex /usr/local/etc/rc.d/nginx <<EOEX
 /^nginx_checkconfig()
 +3
 i
 
 
-	if [ "\$(/usr/local/bin/pluginget gitea_protocol)" = "https" ]; then
-		/usr/local/bin/renderconfig \\
-			/usr/local/share/gitea/nginx/nginx.conf.https.in \\
-			/usr/local/etc/nginx/nginx.conf
-	else
-		/usr/local/bin/renderconfig \\
-			/usr/local/share/gitea/nginx/nginx.conf.http.in \\
-			/usr/local/etc/nginx/nginx.conf
-	fi
+	nginx_mode="\$(/usr/local/bin/plugin config get gitea_nginx_mode || echo '')"
+	case "\$nginx_mode" in
+		https|http)
+			/usr/local/bin/plugin template render \\
+				"/usr/local/share/gitea/nginx/nginx.conf.\${nginx_mode}.in" \\
+				/usr/local/etc/nginx/nginx.conf || return 1
+			;;
+		*)
+			echo "nginx_mode could not be determined; gitea_nginx_mode=\$nginx_mode" >&2
+			return 1
+			;;
+	esac
 .
 wq!
 EOEX
